b259a3fc9be7c21691ea8ff94522ff791daab5b4
Kent Pedrocha <101266723@georgebrown.ca>
2021-11-02 13:33:54 -0400

    initial_project

---
+++ assignment/src/main/java/ca/gbc/comp3095_assignment/Comp3095AssignmentApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3095AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp3095AssignmentApplication.class, args);
+    }
+
+}
---
+++ assignment/src/test/java/ca/gbc/comp3095_assignment/Comp3095AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3095AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
